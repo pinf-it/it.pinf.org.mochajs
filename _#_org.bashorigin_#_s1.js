12fbbbd2d072bf63c83fa518da8068681a782f02
Christoph Dorn <christoph@christophdorn.com>
2017-12-01 18:16:11 -0800

    wip

--- _#_org.bashorigin_#_s1.js
+++ _#_org.bashorigin_#_s1.js
@@ -14,6 +14,7 @@ const MIME_TYPES = require("mime-types");
 const CODEBLOCK = require("codeblock");
 const LODASH = require("lodash");
 const BO = require("bash.origin");
+const CRYPTO = require("crypto");
 
 
 var reports = {};
@@ -27,6 +28,7 @@ exports.forConfig = function (CONFIG) {
         try {
 
             var tests = [];
+
             if (CONFIG.tests) {
                 Object.keys(CONFIG.tests).map(function (testName) {
                     var test = CODEBLOCK.compile(CONFIG.tests[testName]);
@@ -38,25 +40,36 @@ exports.forConfig = function (CONFIG) {
                     ].join("\n"));
                 });
             }
-            
+
+            if (!tests.length) {
+                return callback(null, "");
+            }
+
             if (CONFIG.rootFormat === "pinf") {
 
                 var code = FS.readFileSync(PATH.join(__dirname, "www/index.js"), "utf8");
 
                 code = code.replace(/%%LIB_BASE_URL%%/g, "/.lib");
                 code = code.replace(/%%TESTS%%/g, tests.join("\n"));
-                code = code.replace(/%%STOP_URL%%/g, "http://127.0.0.1:" + CONFIG.variables.PORT + "/stop");
+
+                var apiBaseUrl = CONFIG.apiBaseUrl || "";
+
+                code = code.replace(/%%STOP_URL%%/g, "http://127.0.0.1:" + CONFIG.variables.PORT + apiBaseUrl + "/stop");
                 code = code.replace(/%%SUITE%%/g, CONFIG.suite || "");
 
-                var path = PATH.join(__dirname, ".rt/it.pinf.org.mochajs", process.cwd().replace(/\//g, "~"), "index.js");
+                var hash = CRYPTO.createHash("sha1").update(code).digest('hex').substring(0, 7);
+                // TODO: Instead of using 'hash' here use an identifier that refers to the file + codeblock or other ID
+                //       so we can always target the same file and allow browserify to dynamically re-bundle on source changes.
+                var path = PATH.join(__dirname, ".rt/it.pinf.org.mochajs", process.cwd().replace(/\//g, "~"), "index-" + hash + ".js");
 
                 FS.outputFileSync(path, code, "utf8");
 
-                BO.invokeApi(
+                return BO.invokeApi(
                     {
                         "@it.pinf.org.browserify#s1": {
                             "src": path,
                             "dist": CONFIG.dist,
+                            "prime": true,
                             "format": "pinf"
                         }
                     },
@@ -67,6 +80,10 @@ exports.forConfig = function (CONFIG) {
                             if (err) {
                                 return callback(err);
                             }
+
+                            // TODO: Remove on shutdown.
+                            //FS.unlinkFileSync(path, code);
+
                             return callback(null, code);
                         }
                     ]
@@ -80,14 +97,21 @@ exports.forConfig = function (CONFIG) {
                 page = page.replace(/%%TESTS%%/g, tests.join("\n"));
                 page = page.replace(/%%STOP_URL%%/g, "http://127.0.0.1:" + CONFIG.variables.PORT + "/stop");
                 page = page.replace(/%%SUITE%%/g, CONFIG.suite || "");
+
+                return callback(null, page);
             }
 
-            return callback(null, page);
         } catch (err) {
             return callback(err);
         }
     }
 
+
+    var deferred = {};
+    deferred.promise = new Promise(function (resolve, reject) {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });    
 
     if (
         CONFIG.prime &&
@@ -95,11 +119,15 @@ exports.forConfig = function (CONFIG) {
     ) {
         generateRoot(function (err, page) {
             if (err) {
-                console.error(err);
-                return;
+                //console.error(err);
+                deferred.reject(err);
+                throw err;
             }
             FS.outputFileSync(CONFIG.dist, page, "utf8");
+            return deferred.resolve(page);
         });
+    } else {
+        deferred.resolve(null);
     }
 
 
@@ -122,7 +150,145 @@ exports.forConfig = function (CONFIG) {
         });
     }
 
+    var middleware = function (API) {
+        
+        return function (req, res, next) {
+
+            if (process.env.VERBOSE) console.error("[it.pinf.org.mochajs] req:", req.method, req.url);
+
+            if (
+                req.method === "POST" &&
+                req.url === "/stop"
+            ) {
+
+                if (process.env.VERBOSE) console.error("[it.pinf.org.mochajs] req.body:", req.body);
+
+                if (req.body.error) {
+                    
+                    process.stdout.write("<RESULT" + ((req.body.suite) ? ' suite="' + req.body.suite + '"' : "") + " passed=\"false\">\n");
+                    process.stdout.write(req.body.error + "\n");
+                    process.stdout.write("</RESULT>\n");
+
+                    if (process.env.BO_TEST_FLAG_DEV) {
+                        console.log("NOTE: Leaving browser open due to '--dev'.");
+                    } else {
+                        process.exit(1);
+                    }
+
+                } else
+                if (req.body.failure) {
+
+                    process.stdout.write("<FAILURE" + ((req.body.suite) ? ' suite="' + req.body.suite + '"' : "") + ">\n");
+                    process.stdout.write(JSON.stringify(req.body.failure, null, 4) + "\n");
+                    process.stdout.write("</FAILURE>\n");
+
+                    if (process.env.BO_TEST_FLAG_DEV) {
+                        console.log("NOTE: Leaving browser open due to '--dev'.");
+                    } else {
+                        process.exit(1);
+                    }
+
+                } else
+                if (req.body.stats) {
+
+                    if (!reportSuccess) {
+                        process.stdout.write('>>>TEST_IGNORE_LINE:"(start|end|duration)": (".*Z"|[\\d]+),?\s*$<<<\n');
+                        // TODO: Identify result using URI
+                        process.stdout.write("<RESULT" + ((req.body.suite) ? ' suite="' + req.body.suite + '"' : "") + " passed=\"true\">\n");
+                        process.stdout.write(JSON.stringify(req.body, null, 4) + "\n");
+                        process.stdout.write("</RESULT>\n");
+                    }
+                    
+                    if (req.body.stats.failures === 0) {
+
+                        // Test success
+
+                        if (req.body.suite) {
+                            reportSuccess(req.body.suite, req.body.stats);
+                        }
+
+                        exitIfDesired(CONFIG, API);
+
+                    } else {
+                        console.error("[it.pinf.org.mochajs] ERROR: Test failures!");
+                    }
+                } else {
+                    var err = new Error("Request not understood");
+                    console.error(err);
+                    return next(err);
+                }
+
+                res.writeHead(200, {
+                    "Content-Type": "application/json"
+                });
+                return res.end("{}");
+            } else
+            if (
+                req.method === "GET" &&
+                /^\/\.lib\//.test(req.url)
+            ) {
+
+                var path = PATH.join(__dirname, "node_modules", req.url.replace(/^\/\.lib\//, ""));
+
+                res.writeHead(200, {
+                    "Content-Type": MIME_TYPES.lookup(req.url)
+                });
+
+                res.end(FS.readFileSync(path, "utf8"));
+
+                return null;
+    /*                    
+    console.log("package URL", req.url);
+
+                    var path = PATH.join(__dirname, "node_modules", req.url.replace(/^\/\.lib\//, ""));
+
+    console.log("package path", path);
+
+                    return BO.invokeApi(
+                        {
+                            "@it.pinf.org.browserify#s1": {
+                                "src": path,
+                                "format": "standalone"
+                            }
+                        },
+                        "#io.pinf/middleware~s1"
+                    )(req, res, next);
+    */
+            }
+
+            return generateRoot(function (err, page) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (CONFIG.rootFormat === "pinf") {
+                    res.writeHead(200, {
+                        "Content-Type": "application/javascript"
+                    });
+                } else {
+                    res.writeHead(200, {
+                        "Content-Type": "text/html"
+                    });
+                }
+
+                res.end(page);
+                return null;
+            });
+        };
+    };
+
     return {
+        "#io.pinf/middleware~s2": function (API) {
+            
+            return deferred.promise.then(function (code) {
+
+                return middleware(API);
+            });
+        },
+        "#io.pinf/middleware~s1": function (API) {
+
+            return middleware(API);
+        },                    
         "#io.pinf/expect~s1": function (API) {
 
             if (CONFIG.suites) {
@@ -157,95 +323,6 @@ exports.forConfig = function (CONFIG) {
             }
 
             return allSuitesDoneDeferred.promise;
-        },
-        "#io.pinf/middleware~s1": function (API) {
-
-            return function (req, res, next) {
-
-                if (
-                    req.method === "POST" &&
-                    req.url === "/stop"
-                ) {
-
-                    if (!reportSuccess) {
-                        process.stdout.write('>>>TEST_IGNORE_LINE:"(start|end|duration)": (".*Z"|[\\d]+),?\s*$<<<\n');
-                        // TODO: Identify result using URI
-                        process.stdout.write("<RESULT" + ((req.body.suite) ? ' suite="' + req.body.suite + '"' : "") + ">\n");
-                        process.stdout.write(JSON.stringify(req.body, null, 4) + "\n");
-                        process.stdout.write("</RESULT>\n");
-                    }
-
-                    if (req.body.stats.failures === 0) {
-
-                        // Test success
-
-                        if (req.body.suite) {
-                            reportSuccess(req.body.suite, req.body.stats);
-                        }
-
-                        exitIfDesired(CONFIG, API);
-
-                    } else {
-                        console.error("ERROR: Test failures!");
-                    }
-
-                    res.writeHead(200, {
-                        "Content-Type": "application/json"
-                    });
-                    return res.end("{}");
-                } else
-                if (
-                    req.method === "GET" &&
-                    /^\/\.lib\//.test(req.url)
-                ) {
-
-                    var path = PATH.join(__dirname, "node_modules", req.url.replace(/^\/\.lib\//, ""));
-
-                    res.writeHead(200, {
-                        "Content-Type": MIME_TYPES.lookup(req.url)
-                    });
-
-                    res.end(FS.readFileSync(path, "utf8"));
-
-                    return null;
-/*                    
-console.log("package URL", req.url);
-
-                        var path = PATH.join(__dirname, "node_modules", req.url.replace(/^\/\.lib\//, ""));
-
-console.log("package path", path);
-
-                        return BO.invokeApi(
-                            {
-                                "@it.pinf.org.browserify#s1": {
-                                    "src": path,
-                                    "format": "standalone"
-                                }
-                            },
-                            "#io.pinf/middleware~s1"
-                        )(req, res, next);
-*/
-                }
-
-                return generateRoot(function (err, page) {
-                    if (err) {
-                        return next(err);
-                    }
-
-                    if (CONFIG.rootFormat === "pinf") {
-                        res.writeHead(200, {
-                            "Content-Type": "application/javascript"
-                        });
-                    } else {
-                        res.writeHead(200, {
-                            "Content-Type": "text/html"
-                        });
-                    }
-
-                    res.end(page);
-                    return null;
-                });
-            };
         }
     }
 }
--- www/index.js
+++ www/index.js
@@ -26,22 +26,75 @@ mocha.setup('bdd');
 
 mocha.checkLeaks();
 mocha.globals(['jQuery']);
-mocha.run()
-    .on('error', function (err) {
-        console.error(err);
-    })
-    .on('end', function () {
-        $.ajax({
-            type: "POST",
-            url: "%%STOP_URL%%",
-            data: JSON.stringify({
-                "suite": "%%SUITE%%",
-                "stats": this.stats
-            }),
-            headers: {
-                "Accept": "application/json; charset=utf-8",                     
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            dataType: "json"
-        });
+function logError (err) {
+    console.error("mocha error:", err);
+    $.ajax({
+        type: "POST",
+        url: "%%STOP_URL%%",
+        data: JSON.stringify({
+            "suite": "%%SUITE%%",
+            "error": err.stack
+        }),
+        headers: {
+            "Accept": "application/json; charset=utf-8",                     
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        dataType: "json"
     });
+}
+try {
+    mocha.run()
+        .on('error', logError)
+        .on('fail', function (event) {
+
+console.log("moacha fail:", "%%STOP_URL%%", event);
+
+            if (window.__postJSON) {
+                window.__postJSON("%%STOP_URL%%", {
+                    "suite": "%%SUITE%%",
+                    "failure": event.err
+                });
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: "%%STOP_URL%%",
+                    data: JSON.stringify({
+                        "suite": "%%SUITE%%",
+                        "failure": event.err
+                    }),
+                    headers: {
+                        "Accept": "application/json; charset=utf-8",                     
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    dataType: "json"
+                });
+            }
+        })
+        .on('end', function () {
+
+console.log("moacha done:", "%%STOP_URL%%", this.stats);
+
+            if (window.__postJSON) {
+                window.__postJSON("%%STOP_URL%%", {
+                    "suite": "%%SUITE%%",
+                    "stats": this.stats
+                });
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: "%%STOP_URL%%",
+                    data: JSON.stringify({
+                        "suite": "%%SUITE%%",
+                        "stats": this.stats
+                    }),
+                    headers: {
+                        "Accept": "application/json; charset=utf-8",                     
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    dataType: "json"
+                });
+            }            
+        });
+} catch (err) {
+    logError(err);
+}
